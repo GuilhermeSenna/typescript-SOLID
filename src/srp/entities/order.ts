4f9e169a1f81aaa50b777b502591ea70955ad250
zBiggui <bigguisenna@gmail.com>
2022-08-12 18:46:11 -0300

    feat: finish lesson about SRP - Single Responsibility Principle

---
+++ src/srp/entities/order.ts
@@ -0,0 +1,38 @@
+import { orderStatus } from './interfaces/order-status';
+import { Messaging } from '../services/messaging';
+import { Persistency } from '../services/persistency';
+import { ShoppingCart } from './shopping-cart';
+
+export class Order {
+    private _orderStatus: orderStatus = 'open';
+
+    // This is breaking another principle
+    constructor(
+        // Dependency injection
+        private readonly cart: ShoppingCart,
+        private readonly messaging: Messaging,
+        private readonly persistency: Persistency,
+    ) {}
+
+    get orderStatus(): orderStatus {
+        return this._orderStatus;
+    }
+
+    checkout(): void {
+        if (this.cart.isEmpty()) {
+            console.log('Seu carrinho está vazio');
+            return;
+        }
+
+        this._orderStatus = 'closed';
+        this.messaging.sendMessage(
+            `Seu pedido com total de R$ ${this.cart.total()} foi recebido.`,
+        );
+        this.persistency.saveOrder();
+        this.cart.clear();
+    }
+
+    saveOrder(): void {
+        console.log('Pedido salvo com sucesso...');
+    }
+}
