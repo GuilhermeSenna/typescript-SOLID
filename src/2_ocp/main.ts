24f5ca10423ddde7e31f386302cf17a4cd4950f3
zBiggui <bigguisenna@gmail.com>
2022-08-13 16:17:42 -0300

    feat: add classes instances and relations in main.ts

--- src/2_ocp/main.ts
+++ src/2_ocp/main.ts
@@ -1,10 +1,23 @@
-import { Messaging } from './services/messaging';
-import { Order } from './entities/order';
-import { Persistency } from './services/persistency';
-import { Product } from './entities/product';
-import { ShoppingCart } from './entities/shopping-cart';
+/*
+Open/closed principle
+Entidades devem estar abertas para extensão, mas fechadas para modificação.
+*/
 
-const shoppingCart = new ShoppingCart();
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import {
+    FiftyPercentDiscount,
+    NoDiscount,
+    TenPercentDiscount,
+} from './classes/discount';
+
+// const fiftyPercentDiscount = new FiftyPercentDiscount();
+// const tenPercentDiscount = new TenPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
@@ -15,6 +28,7 @@ shoppingCart.addItem(new Product('Lápis', 1.59));
 
 console.log(shoppingCart.items);
 console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
 console.log(order.orderStatus);
 order.checkout();
 console.log(order.orderStatus);
