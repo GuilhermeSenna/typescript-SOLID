453929032d185bda8938341cbd993c537a03ec58
zBiggui <bigguisenna@gmail.com>
2022-08-13 22:55:46 -0300

    feat: pros and cons of SOLID add in index.ts

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+/*
+Vantagens e desvantagens do S.O.L.I.D
+
+Vantagens:
+- Código modular
+- Código reutilizável: (D.R.Y - Don't repeat yourself)
+- Código testável
+- Baixo acoplamento e alta coesão
+- Código escalável
+- Separations of concerns (SRP)
+- Fácil manutenção
+
+Desvantagens:
+- Alta complexidade
+- Aumento na quantidade de código
+- Tempo de desenvolvimento aumenta bastante
+- Necessário cuidados com:
+    - YAGNI (you aren't gonna need it) <- Fazer do jeito que sabe depois refatorar
+    - KISS (keep it simple, stupid)
+    - MVP (Minimum viable product)
+*/
